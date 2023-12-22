c325c3d9354ee4ea87b93303c4c6adf5d21b3596
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2023-12-22 08:16:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from './components/Navbar'
+import Filter from './components/Filter'
+import Cards from './components/Cards'
+import { apiUrl, filterData } from './data';
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import Spinner from "./components/Spinner"
 
-function App() {
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      // output -> \
+      setCourses(output.data);
+    }
+    catch (error) {
+      toast.error("Network Problem")
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col bg-[#40445c]">
+      <div>
+        <Navbar />
+      </div>
+
+        <div>
+          <Filter filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+          />
+        </div>
+
+        <div className='w-11/12 max-w-[1200px] mx-auto flex 
+          justify-center items-center min-h[50vh] flex-wrap  '>
+          {
+            loading ? (<Spinner />) : (<Cards courses={courses} category={category} />)
+          }
+        </div>
+   
     </div>
   );
 }
